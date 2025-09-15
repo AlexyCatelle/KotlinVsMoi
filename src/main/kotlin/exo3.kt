d2b4afb4711abd0e1e185e201f0a5b09e8df4c8a
AlexyCatelle <alexy.catelle@oclock.school>
2025-09-15 16:24:36 +0200

    exo 3

--- src/main/kotlin/exo3.kt
+++ src/main/kotlin/exo3.kt
@@ -1,3 +1,10 @@
+import jdk.internal.vm.vector.VectorSupport.test
+
 fun quelleHeure(heure : String ="12h00"){
-    
+    println("Il est $heure")
+}
+
+fun main(){
+    quelleHeure();
+    quelleHeure("14h00");
 }
