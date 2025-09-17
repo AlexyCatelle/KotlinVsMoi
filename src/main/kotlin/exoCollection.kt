1c560a19a8ce91000ec676e99b6f9fd11c6b89ee
AlexyCatelle <alexy.catelle@oclock.school>
2025-09-17 11:10:41 +0200

    collection cas 2

--- src/main/kotlin/exoCollection.kt
+++ src/main/kotlin/exoCollection.kt
@@ -1,10 +1,11 @@
 fun main (){
 inscription()
+    file()
 }
 
 // Cas 1 : Inscription
 fun inscription(){
-    var joueurs = sortedSetOf("Tutu", "Tata")
+    val joueurs = sortedSetOf("Tutu", "Tata")
     joueurs.add("Tutu")
     joueurs.add("Tata")
     joueurs.add("Titi")
@@ -18,11 +19,26 @@ fun inscription(){
     println("Le joueur $search est-il inscrit ? ${search in joueurs}")
 }
 
-
 // Cas 2 : File d'attente
 fun file(){
 
-}
+    val clients = ArrayDeque<String>()
+
+    clients.add("Tutu")
+    clients.add("Tata")
+    clients.add("Titi")
+    clients.add("Titi")
+    clients.add("Toto")
+
+    while (!clients.isEmpty()) {
+        println("Appuyez sur Entrée pour traiter le prochain client...")
+        readLine() // on attend que l'utilisateur appuie sur Entrée
+        val client = clients.removeFirst()
+        println("En cours de traitement : $client")
+        println("Clients restants : ${clients.size}")
+    }
+    println("C'est l'heure de la pause.")
+    }
 
 // Cas 3 : Gestion de notes
 fun gestion(){
