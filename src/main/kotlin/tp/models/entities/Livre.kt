c455f1dd619efa288a667cdce51d53857db791ec
AlexyCatelle <alexy.catelle@oclock.school>
2025-09-17 14:56:16 +0200

    4 ok

--- src/main/kotlin/tp/models/entities/Livre.kt
+++ src/main/kotlin/tp/models/entities/Livre.kt
@@ -1,19 +1,33 @@
 package tp.models.entities
 
 import tp.models.enums.Genre
+import tp.models.interfaces.Empruntable
 
 class Livre(
     titre : String,
     annneePublication: Int,
     auteurice: String,
     nbPages: Int,
-    Genre: Genre)
-    : Document(titre, annneePublication) {
+    Genre: Genre,
+    override var estEmprunte: Boolean = false)
+
+    : Document(titre, annneePublication), Empruntable {
+
+
+
 
     constructor(titre: String, auteurice: String, genre: Genre) :
             this(titre, 0, auteurice, 100, genre)
 
     override fun afficherInfos() {
+        TODO("Not yet implemented")
+    }
+
+    override fun emprunter() {
+        TODO("Not yet implemented")
+    }
+
+    override fun rendre() {
         TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/tp/models/interfaces/Empruntable.kt
+++ src/main/kotlin/tp/models/interfaces/Empruntable.kt
@@ -1,4 +1,8 @@
 package tp.models.interfaces
 
 interface Empruntable {
+    var estEmprunte:Boolean
+
+    fun emprunter()
+    fun rendre()
 }
